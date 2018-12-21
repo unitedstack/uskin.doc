1da2e839b188fae653920c3df6945945766b7ac6
PengJiyuan <731401082@qq.com>
2018-12-21 15:56:40 +0800

    add image

--- pages/home/model.jsx
+++ pages/home/model.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import loginBg from './assets/loginBg.png';
 
-const Model = () => <h1>This is Home Page! <a href="docs.html">组件页</a></h1>;
+const Model = () => (
+  <div>
+    <h1>This is Home Page! <a href="docs.html">组件页</a></h1>
+    <img src={loginBg} alt="loginBg" />
+  </div>
+);
 
 export default Model;
